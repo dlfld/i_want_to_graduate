83e5d845e0a42db6c40c1ffeb9ce0dce2602c901
戴林峰 <dailinfeng66@163.com>
2023-05-05 14:41:49 +0800

    update

--- preprocess/test/main.py
+++ preprocess/test/main.py
@@ -1,8 +1,15 @@
-from itertools import product,permutations
-
 if __name__ == '__main__':
-    list1 = [1, 2, 3, 4]
-    list2 = [1, 2, 3, 4]
-    res = list(product(list1, repeat=2))
-    for item in res:
-        print(list(item)+[1])
+    code = """
+    class Test{
+        public AjaxResult getInfo() throws Exception
+        {
+            Server server = new Server();
+            server.copyTo();
+            return AjaxResult.success(server);
+        }
+    }
+
+    """
+    import javalang
+
+    print(javalang.parse.parse(code))
